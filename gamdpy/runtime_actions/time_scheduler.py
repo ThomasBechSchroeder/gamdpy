6b1c771397540c8f3efd9d5a65b264996e5c82f5
Michele Matteucci <michele.matteucci@phd.units.it>
2025-06-19 19:23:34 +0200

    Compute flags and save indexes inside stepcheck functions

--- gamdpy/runtime_actions/time_scheduler.py
+++ gamdpy/runtime_actions/time_scheduler.py
@@ -31,7 +31,8 @@ class TimeScheduler():
 
     def __init__(self, schedule='log2', **kwargs):
 
-        self.known_schedules = ['log2', 'log', 'lin', 'geom', 'custom']
+        self.known_schedules = ['log2', 'log', 'lin', 'geom']
+        # self.known_schedules = ['log2', 'log', 'lin', 'geom', 'custom']
         self.schedule = schedule
         self._kwargs = kwargs
 
@@ -46,111 +47,139 @@ class TimeScheduler():
 
         # no specific kwarg is required
         if self.schedule=='log2':
-            self.base = 2
-            self.steps = self._steps_log()
+            self.stepcheck_func = self._get_stepcheck_log2()
 
-        # `base` kwarg is accepted but not required
+        # `base` kwarg is accepted but not required (default is Euler number)
         elif self.schedule=='log':
             self.base = self._kwargs.get('base', np.exp(1.0))
-            self.steps = self._steps_log()
+            self.stepcheck_func = self._get_stepcheck_log()
 
-        # `steps_between_output` or `npoints` kwarg is required
+        # `steps_between_output` kwarg is required
         elif self.schedule=='lin':
             deltastep = self._kwargs.get('steps_between_output', None)
             npoints = self._kwargs.get('npoints', None)
             if deltastep is not None:
                 self.deltastep = deltastep
-                self.steps = self._steps_lin(delta=True)
+                self.stepcheck_func = self._get_stepcheck_lin()
             elif npoints is not None:
-                self.npoints = npoints
-                self.steps = self._steps_lin(delta=False)
+                raise NotImplementedError("Passing 'npoints' to 'lin' schedule should be possible")
+                #self.npoints = npoints
             else:
                 raise TypeError("'steps_between_output' or 'npoints' is required for schedule 'lin'")
 
         # `npoints` kwarg is required
         elif self.schedule=='geom':
-            npoints = self._kwargs.get('npoints', None)
+            self.npoints = self._kwargs.get('npoints', None)
             if npoints is not None:
                 self.npoints = npoints
-                self.steps = self._steps_geom()
-                assert self.nsaves==self.npoints, 'Too many points, schedule distorsion; try fewer points'
-            else:
-                raise TypeError("'npoints' is required for schedule 'geom'")
+                self.stepcheck_func = self._get_stepcheck_geom()
 
-        # `steps` kwarg is required
-        elif self.schedule=='custom':
-            steps = self._kwargs.get('steps', None)
-            if steps is not None:
-                assert isinstance(steps, (list, np.array)), "'steps' must be 'list' or 'numpy.array'"
-                self.steps = np.unique(np.array(steps))
-            else:
-                raise TypeError("'steps' is required for schedule 'custom'")
+        # TODO: using custom steps would require passing arrays to stepcheck functions, which is a potential issue
+        # elif self.schedule=='custom':
+        #     pass
 
-        if self.schedule != 'log2':
-            self.stepcheck_func = self._get_stepcheck()
-        else:
-            self.stepcheck_func = self._get_stepcheck_log2()
+        self.steps = self._compute_steps()
+        # self.stepsall = self._compute_stepsall()
+        if self.schedule=='geom':
+            # the 'geom' schedule must return each save index only once; this must be after _compute_steps()
+            assert self.nsaves==self.npoints, 'Too many points, schedule distorsion; try fewer points'
 
-    def _steps_log(self):
-        steps = [0]
-        step = 0
-        exponent = 0
-        while True:
-            step = int(self.base**exponent)
-            if step>self.stepmax:
-                break
-            steps.append(step)
-            exponent += 1
-        return np.unique(np.array(steps))
-        
-    def _steps_lin(self, delta):
-        steps = [0]
-        if delta:
-            step = 0
-            while True:
-                step += self.deltastep
-                if step>=self.stepmax:
-                    break
-                steps.append(step)
-        else:
-            delta = self.stepmax / self.npoints
-            time = 0
-            while True:
-                time += delta
-                if time>=self.stepmax:
-                    break
-                steps.append(int(time))
-        return np.unique(np.array(steps))
-
-    def _steps_geom(self):
-        steps = [0]
-        a = self.stepmax**(1.0/self.npoints)
-        for i in range(1, self.npoints):
-            step = int(a**(i+1) - 1)
-            steps.append(step)
-        return np.unique(np.array(steps))
-
-    def _get_stepcheck(self):
-        steps = np.array(self.steps)
-        def stepcheck(step):
-            for j in range(len(steps)):
-                if step==steps[j]:
-                    return True, j
-            return False, -1
-        return stepcheck
-    
     def _get_stepcheck_log2(self):
         def stepcheck(step):
+            flag = False
+            idx = -1 # this is for python calls of the function
             if step == 0:
-                return True, 0
+                flag = True
+                idx = 0
             else:
                 b = np.int32(math.log2(np.float32(step)))
                 c = 2 ** b
-                if step==c:
-                    return True, b+1
+                if step == c:
+                    flag = True
+                    idx = b + 1
+            return flag, idx
+        return stepcheck
+
+    def _get_stepcheck_log(self):
+        base = self.base
+        def stepcheck(step):
+            if step==0 or step==1:
+                return True, step
+            prev_int = 1
+            current = 1.0
+            idx = 1
+            while True:
+                current *= base
+                curr_int = int(current)
+                if curr_int != prev_int:
+                    idx += 1
+                    if curr_int == step:
+                        return True, idx
+                    if curr_int > step:
+                        break
+                    prev_int = curr_int            
             return False, -1
         return stepcheck
+
+    def _get_stepcheck_lin(self):
+        deltastep = self.deltastep
+        def stepcheck(step):
+            if step%deltastep==0:
+                return True, step//deltastep
+            return False, -1
+        return stepcheck
+
+    def _get_stepcheck_geom(self):
+        stepmax = self.stepmax
+        npoints = self.npoints
+        def stepcheck(step):
+            if step==0:
+                return True, 0
+            xx = stepmax**(1.0/npoints)
+            for idx in range(1, npoints):
+                c = xx**(idx+1)-1
+                if step==int(c):
+                    return True, idx
+            return False, -1
+        return stepcheck
+
+    def _compute_steps(self):
+        steps = []
+        # we want to include the last step IFF it raises a True flag
+        for step in range(self.stepmax+1):
+            flag, _ = self.stepcheck_func(step)
+            if flag: steps.append(step)
+        # if stepmax not in steps: steps.append(stepmax)
+        return np.unique(np.array(steps))
 
     @property
     def nsaves(self):
         return len(self.steps)
+
+    # def _compute_stepsall(self):
+    #     try:
+    #         stepmax = self.stepmax
+    #         ntimeblocks = self.ntimeblocks
+    #     except AttributeError:
+    #         print('probably setup() has not been called yet')
+    #     steps = []
+    #     for step in range(stepmax+1):
+    #         flag, _ = self.stepcheck_func(step)
+    #         if flag: steps.append(step)
+    #     overallsteps = []
+    #     for i_block in range(ntimeblocks):
+    #         for step in steps:
+    #             overallstep = step+stepmax*i_block
+    #             overallsteps.append(overallstep)
+    #     if stepmax not in steps: 
+    #         overallsteps.append(stepmax*ntimeblocks)
+    #     return overallsteps
+
+
+    # @property
+    # def nsavesall(self):
+    #     try:
+    #         return len(self.stepsall)
+    #     except AttributeError:
+    #         print('probably setup() has not been called yet')
+
